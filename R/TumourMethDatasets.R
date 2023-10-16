e0417403b3bb4193137daded3d649c6839d486a9
RICH <rich@rich-Lenovo-ideapad-330S-14IKB>
2023-10-16 11:10:07 +0200

    Added function to download datasets from TumourMethData

---
+++ R/TumourMethDatasets.R
@@ -0,0 +1,27 @@
+TumourMethDatasets = data.frame(
+  dataset_name = c("cpgea_wgbs_hg38", "tcga_wgbs_hg38", "mcrpc_wgbs_hg38", "mcrpc_wgbs_hg38_chr11", "cao_esophageal_wgbs_hg19"), 
+  cancer_type = c("prostate", "various", "prostate", "prostate", "esophageal"),
+  technology = c("WGBS", "WGBS", "WGBS", "WGBS", "WGBS"),
+  genome_build = c("hg38", "hg38", "hg38", "hg38", "hg19"),
+  number_tumour_samples = c(187, 39, 100, 100, 10), 
+  number_normal_samples = c(187, 8, 0, 0, 9),
+  wgbs_coverage_available = c(FALSE, FALSE, TRUE, TRUE, FALSE),
+  dataset_size_gb = c(40, 5.4, 16, NA, 2),
+  transcript_counts_available = c(TRUE, TRUE, TRUE, TRUE, TRUE), 
+  notes = c("", "", "", "This dataset is a subset of the data in mcrpc_wgbs_hg38 for example pruposes", ""), 
+  original_publication = c(
+    "A genomic and epigenomic atlas of prostate cancer in Asian populations; Nature; 2020", 
+    "DNA methylation loss in late-replicating domains is linked to mitotic cell division; Nature genetics; 2018",
+    "The DNA methylation landscape of advanced prostate cancer; Nature genetics; 2020",
+    "The DNA methylation landscape of advanced prostate cancer; Nature genetics; 2020",
+    "Multi-faceted epigenetic dysregulation of gene expression promotes esophageal squamous cell carcinoma; Nature communications; 2020"
+  )
+)
+usethis::use_data(TumourMethDatasets, overwrite = T)
+
+.experimenthub_ids = data.frame(
+  wgbs = c("EH8524", "EH8425", "EH8526", "EH8527", "EH8528"),
+  rnaseq = c("", "", "", "", ""), 
+  row.names = c("tcga_wgbs_hg38", "mcrpc_wgbs_hg38", "cpgea_wgbs_hg38", "cao_esophageal_wgbs_hg19", "mcrpc_wgbs_hg38_chr11") 
+)
+usethis::use_data(.experimenthub_ids, overwrite = T, internal = T)
---
+++ R/download_datasets.R
@@ -0,0 +1,63 @@
+#' Download a WGBS methylation dataset from TumourMethData
+#'
+#' @param dataset Name of the dataset to download. Must be one of the datsets listed in data(TumourMethDatasets). 
+#' @param dir Parent directory to create links to the HDF5 SummarizedExperiment dataset. 
+#' A subdirectory with the dataset name will be created within this directory. Default is tempdir().
+#' @return A RangedSummarizedExperiment with methylation values from the specified dataset. 
+#' @export
+#' @examples
+#' tcga_wgbs_hg38 = TumourMethData::download_dataset(dataset = "tcga_wgbs_hg38", dir = ".")
+#' print(tcga_wgbs_hg38)
+download_meth_dataset = function(dataset, dir = tempdir()){
+  
+  # Load TumourMethDatasets
+  data("TumourMethDatasets", package = "TumourMethData")
+  
+  # Check that dataset is one of the allowed options
+  if(!dataset %in% TumourMethDatasets$dataset_name){
+    stop("dataset should be one of the dataset names in TumourMethDatasets")
+  }
+  
+  # Create output_dir from dir and dataset name
+  output_dir = paste(dir, dataset, sep = "/")
+  
+  # Check if output_dir already exists
+  if(!dir.exists(dir)){stop("dir doesn't exist")}
+  if(dir.exists(output_dir)){stop(
+    paste(output_dir, "already exists")
+  )}
+  
+  # Extract the appropriate EH ID for the dataset
+  eh_id = .experimenthub_ids[dataset, "wgbs"]
+  
+  # Create a connection to ExperimentHub and find the entry for the specified dataset
+  eh  = ExperimentHub::ExperimentHub()
+  dataset_files = eh[[eh_id]]
+  
+  # Check that two files were downloaded
+  if(length(dataset_files) != 2){
+    stop(paste("There were", length(dataset_files), "files downloaded, however there should be only 2 files per dataset"))
+  }
+  
+  # Identify H5 file
+  h5_file = dataset_files[which(sapply(dataset_files, function(x)
+    tryCatch({rhdf5::h5ls(x); TRUE}, error = function(e) FALSE)))]
+  rds_file = dataset_files[which(sapply(dataset_files, function(x)
+    tryCatch({readRDS(x); TRUE}, error = function(e) FALSE)))]
+  
+  # Check that one H5 file and one RDS file were found
+  if(length(h5_file) == 0){stop("No HDF5 file was downloaded")}
+  if(length(rds_file) == 0){stop("No RDS file was downloaded")}
+  
+  # Create directory and create symlinks to files in it
+  dir.create(output_dir)
+  h5_link = paste(output_dir, "assays.h5", sep = "/")
+  rds_link = paste(output_dir, "se.rds", sep = "/")
+  R.utils::createLink(link = h5_link, target = h5_file)
+  R.utils::createLink(link = rds_link, target = rds_file)
+  
+  # Create RangedSummarizedExperiment from output_dir
+  rse = HDF5Array::loadHDF5SummarizedExperiment(output_dir)
+  return(rse)
+  
+}
